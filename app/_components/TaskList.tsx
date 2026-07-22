2205e4ede440b17539a3ded1e91febd523ecaa90
Seva Kurlovich <kurlovichusevalad@gmail.com>
2026-07-22 11:47:57 +0200

    8: Change TaskList accordingly to cooperate with TaskManager

--- app/_components/TaskList.tsx
+++ app/_components/TaskList.tsx
@@ -1,16 +1,11 @@
 import Task from './Task';
-import { TaskStatus } from './Task';
+import { TaskProps } from './Task';
 
-const tasks = [
-    {id: 0, title: "Get Up", description: "Don't turn the alarm off", status: "todo" as TaskStatus},
-    {id: 1, title: "Take A Shower", description: "Make sure there is water", status: "in-progres" as TaskStatus},
-    {id: 2, title: "Make Breakfast", description: "Break a few eggs to make an omlette", status: "todo" as TaskStatus},
-    {id: 3, title: "Clean the Room", description: "No more dust and dirt", status: "todo" as TaskStatus},
-    {id: 4, title: "Go outside", description: "Touch some grass", status: "todo" as TaskStatus},
-    {id: 5, title: "Read a Book", description: "Chapter 1..", status: "in-progress" as TaskStatus},
-]
+interface TaskListProps {
+    tasks: TaskProps[]; 
+}
 
-export default function TaskList(){
+export default function TaskList({tasks}: TaskListProps){
     if (tasks.length <= 0){
         return <p>No tasks on the board</p>
     }
