3412bc8edf430ffd89b8e2d1ee00d2c28c2cf01c
Seva Kurlovich <kurlovichusevalad@gmail.com>
2026-07-22 11:55:34 +0200

    9: Fix a filtering bug, where nothing happens

--- app/_components/TaskManager.tsx
+++ app/_components/TaskManager.tsx
@@ -8,7 +8,7 @@ import TaskList from './TaskList';
 
 const initialTasks = [
     {id: 0, title: "Get Up", description: "Don't turn the alarm off", status: "todo" as TaskStatus},
-    {id: 1, title: "Take A Shower", description: "Make sure there is water", status: "in-progres" as TaskStatus},
+    {id: 1, title: "Take A Shower", description: "Make sure there is water", status: "in-progress" as TaskStatus},
     {id: 2, title: "Make Breakfast", description: "Break a few eggs to make an omlette", status: "todo" as TaskStatus},
     {id: 3, title: "Clean the Room", description: "No more dust and dirt", status: "todo" as TaskStatus},
     {id: 4, title: "Go outside", description: "Touch some grass", status: "todo" as TaskStatus},
