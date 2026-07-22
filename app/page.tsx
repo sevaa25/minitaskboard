0e9197c5b90b5f492b662b962a3853c6dbd53cfa
Seva Kurlovich <kurlovichusevalad@gmail.com>
2026-07-22 11:25:02 +0200

    4: Add elements/components to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import SearchBar from "./_components/SearchBar";
 import TaskList from "./_components/TaskList";
 
 export default function Home() {
   return (
     <>
+      <h1>My Tasks for today: </h1>
+      <SearchBar/>
       <TaskList/>
     </>
   );
