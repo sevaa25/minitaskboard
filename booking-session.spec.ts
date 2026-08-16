024876016058eaa3b13c3e61ea1689bbb8db1fa7
Seva Kurlovich <kurlovichusevalad@gmail.com>
2026-08-16 23:09:29 +0200

    Old tests updated, new tests added.

--- booking-session.spec.ts
+++ booking-session.spec.ts
@@ -1,28 +1,75 @@
 import { test, expect } from '@playwright/test';
-test.setTimeout(40000);
+
+test.setTimeout(50000);
+
 test.describe('Booking a session / entering wait list', () => {
   test.beforeEach( async ({page})=> {
     await page.goto("https://staging.wellbee.pl/en");
     await page.getByRole('link', {name: "BOOK A SESSION"}).click();
     await expect(page).toHaveURL("https://staging.wellbee.pl/en/znajdz-terapeute");
+
   });
 
   test('Individual therapy', async ({page}) => {
     await page.getByRole('button', {name: "Individual therapy"}).click();
     await expect(page).toHaveURL("https://staging.wellbee.pl/en/znajdz-terapeute/terapia-indywidualna/obszar-wsparcia");
     await page.locator('div[role="button"].MuiChip-root').first().click();
+
     const selectedTopics = page.locator('div[role="button"].MuiChip-colorPrimary');
     await expect(selectedTopics).not.toHaveCount(0);
     await page.getByRole('button', {name: "Next"}).click();
+
     await expect(page).toHaveURL("https://staging.wellbee.pl/en/znajdz-terapeute/terapia-indywidualna/styl1");
     await page.getByRole('button').nth(1).click();
-    await expect(page).toHaveURL("https://staging.wellbee.pl/en/znajdz-terapeute/terapia-indywidualna/styl2");
+    await expect(page).toHaveURL("https://staging.wellbee.pl/en/znajdz-terapeute/terapia-indywidualna/styl2");  
     await page.getByRole('button').nth(1).click();
+
     await page.getByRole("button", {name: "Search"}).click();
     await page.getByPlaceholder("Search").fill("endtoendtest endtoendtest");
     await page.getByRole('link').filter({hasText: "endtoendtest endtoendtest"}).click();
+
     await expect(page).toHaveURL("https://staging.wellbee.pl/en/nasi-specjalisci/endtoendtest-endtoendtest?bookingFlow=true");
     await page.getByRole('button', {name: "JOIN THE WAITING LIST"}).click();
+
+    const dialog = page.getByRole("dialog");
+    await expect(dialog).toBeVisible();
+    await dialog.getByPlaceholder('E-mail').fill('testemailtest@gmail.com');
+    await dialog.getByPlaceholder('732108206').fill('123456789');
+    await expect(async () => {
+      await dialog.locator('label').filter({ hasText: 'I acknowledge that my data' }).click();
+    }).toPass();
+    await dialog.getByRole('button', {name: "Send"}).click();
+    await expect(dialog).toBeHidden();
+  });
+
+  test('Couples Therapy', async ({page}) => {
+    await page.getByRole('button', {name: "Couples Therapy"}).click();
+    
+    await page.getByRole("button", {name: "Search"}).click();
+    await page.getByPlaceholder("Search").fill("John Wick");
+    await page.getByRole('button', {name: "Book a session"}).click();
+    await expect(page).toHaveURL("https://staging.wellbee.pl/en/google.pl");
+
+    const errorMessage =  page.getByText("Ups!");
+    await expect(errorMessage).toBeVisible();
+
+  })
+
+  test('Children/young people', async ({page}) =>{
+    await page.getByRole('button', {name : "Children/young people"}).click();
+    await page.getByRole('button', {name: "Parental consultations"}).click();
+
+    await expect(page).toHaveURL("https://staging.wellbee.pl/en/znajdz-terapeute/dzieci-mlodziez/konsultacje-rodzicielskie/styl1")
+    await page.getByRole('button').nth(1).click();
+    await expect(page).toHaveURL("https://staging.wellbee.pl/en/znajdz-terapeute/dzieci-mlodziez/konsultacje-rodzicielskie/styl2")
+    await page.getByRole('button').nth(1).click();
+
+    await page.pause();
+
+    await page.getByRole("button", {name: "Search"}).click();
+    await page.getByPlaceholder("Search").fill("Natalia Pilarska- Długie");
+    await page.getByRole('button', {name: "Join the waiting list"}).click();
+
     const dialog = page.getByRole("dialog");
     await expect(dialog).toBeVisible();
     await dialog.getByPlaceholder('E-mail').fill('testemailtest@gmail.com');
@@ -33,7 +80,9 @@ test.describe('Booking a session / entering wait list', () => {
     await dialog.getByRole('button', {name: "Send"}).click();
     await expect(dialog).toBeHidden();
 
-  });
+  })
 
 
-});
+
+}); 
+
